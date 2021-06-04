289a98e50329bb546ed391dfe519741e7d981dfc
Albert Zeyer <albzey@gmail.com>
2021-06-04 17:58:25 +0200

    some first base interface POC, WIP

--- models/base.py
+++ models/base.py
@@ -3,7 +3,7 @@ Base interfaces
 """
 
 from __future__ import annotations
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 
 
 LayerDictRaw = Dict[str, Any]
@@ -43,7 +43,8 @@ class ILayerMaker:
 class Module(ILayerMaker):
   """
   Like PyTorch.
-  This represents a subnetwork in RETURNN.
+  This represents a subnetwork in RETURNN, or the root network.
+  Or some other layer which has a subnetwork, like RecLayer.
   """
 
   def call(self, *args, **kwargs) -> Layer:
@@ -55,3 +56,23 @@ class Module(ILayerMaker):
   def __call__(self, *args, **kwargs) -> Layer:
     # TODO...
     return self.call(*args, **kwargs)
+
+  def make_root_net(self) -> _NameCtx:
+    """
+    Make net.
+    """
+    assert not _NameCtx.stack
+    ctx = _NameCtx(module=self, layer=None)
+    # TODO
+    layer = self()
+    return ctx
+
+
+class _NameCtx:
+  stack = []  # type: List[_NameCtx]
+
+  def __init__(self, *, module: Optional[Module], layer: Optional[Layer]):
+    self.layer = layer
+    self.module = module
+    self.childs = {}  # type: Dict[str, _NameCtx]
+    self.stack.append(self)
