afe9f98c458e6a07d72a0cfea25daf5b61e801b7
Albert Zeyer <albzey@gmail.com>
2021-06-04 11:26:18 +0200

    copy tests setup from returnn

---
+++ tests/test_hello.py
@@ -0,0 +1,9 @@
+
+import _setup_test_env  # noqa
+from nose.tools import assert_equal, assert_is_instance, assert_in, assert_greater, assert_true, assert_false
+
+
+def test_hello():
+  from returnn_common.tests.hello import hello
+  assert_equal(hello(), "hello world")
+
