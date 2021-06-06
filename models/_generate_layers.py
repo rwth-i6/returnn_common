bc56fb441955edc5d3d0fbaa5734bc6da92dc5d3
Albert Zeyer <albzey@gmail.com>
2021-06-06 12:44:41 +0200

    prepare for automatic layer wrappers

---
+++ models/_generate_layers.py
@@ -0,0 +1,13 @@
+"""
+Importing this has the side-effect of generating the wrapper ``_generated_layers.py``
+"""
+
+
+def setup():
+  """
+  Setup
+  """
+  pass
+
+
+setup()
---
+++ models/layers.py
@@ -0,0 +1,6 @@
+"""
+Wrap RETURNN layers
+"""
+
+from . import _generate_layers  # noqa
+from ._generated_layers import *  # noqa
