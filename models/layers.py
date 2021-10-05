1b5fcad024836a3ca22a97cb99f141ef578c9dfc
Albert Zeyer <albzey@gmail.com>
2021-10-05 17:19:26 +0200

    split function

--- models/layers.py
+++ models/layers.py
@@ -6,6 +6,24 @@ from ._generated_layers import *  # noqa
 from .base import Module  # noqa
 
 
+def split(source: LayerRef, *,
+          axis: Optional[str] = NotSpecified,
+          num_splits: Optional[int] = NotSpecified,
+          size_splits: Optional[List[int]] = NotSpecified,
+          ) -> Tuple[LayerRef, ...]:
+  """
+  Split the input on the specified axis (by default feature).
+  Basically a wrapper around tf.split.
+  """
+  from ._generated_layers import _split
+  from .base import get_sub_layer
+  res = _split(source, axis=axis, num_splits=num_splits, size_splits=size_splits)
+  if num_splits is None:
+    assert isinstance(size_splits, (tuple, list))
+    num_splits = len(size_splits)
+  return tuple(get_sub_layer(res, str(i)) for i in range(num_splits))
+
+
 class Lstm(Rec):
   """
   LSTM
