a5dcdc65c6f401f8bc233064a6a55105ed003558
Albert Zeyer <albzey@gmail.com>
2021-10-08 16:42:01 +0200

    test_rec_simple_iter

--- tests/test_models_rec.py
+++ tests/test_models_rec.py
@@ -33,3 +33,24 @@ def test_rec_ff():
   net_dict = net.make_root_net_dict()
   pprint(net_dict)
   dummy_run_net(net_dict)
+
+
+def test_rec_simple_iter():
+  class _Net(Module):
+    def forward(self) -> LayerRef:
+      """
+      Forward
+      """
+      x = get_extern_data("data")
+      # https://github.com/rwth-i6/returnn_common/issues/16
+      with Loop(max_seq_len=10) as loop:
+        loop.state.i = State()
+        loop.state.i = loop.state.i + 1.
+        loop.end(loop.state.i >= 5.)
+        y = loop.stack(loop.state.i * reduce(x, mode="mean", axis="T"))
+      return y
+
+  net = _Net()
+  net_dict = net.make_root_net_dict()
+  pprint(net_dict)
+  dummy_run_net(net_dict)
