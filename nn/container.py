0eb402beafa55132b3172e8581207a8ea6cd528e
Albert Zeyer <albzey@gmail.com>
2021-10-27 16:26:32 +0200

    module list, sequential, generalize

--- nn/container.py
+++ nn/container.py
@@ -4,7 +4,7 @@ container functions
 
 from __future__ import annotations
 from .base import Module, ILayerMaker, LayerRef
-from typing import Iterable, Iterator, Optional, Union, Dict, Callable
+from typing import Iterable, Iterator, Union, Dict, Callable
 
 
 _UnaryFuncT = Callable[[LayerRef], LayerRef]
@@ -15,13 +15,22 @@ class ModuleList(Module):
   """
   Module list, getting passed an Iterable of Modules and creates a list of Modules in that order
   """
-  def __init__(self, modules: Optional[Iterable[_ModT]] = None):
+  def __init__(self, *modules: Union[_ModT, Iterable[_ModT], Dict[str, _ModT], ModuleList]):
     super().__init__()
-    if modules is not None:
+    if len(modules) == 1 and isinstance(modules[0], dict):
+      for key, module in modules[0].items():
+        setattr(self, key, _convert_to_maker(module))
+    elif len(modules) == 1 and isinstance(modules[0], ModuleList):
+      for key, module in modules[0]._get_makers().items():
+        setattr(self, key, _convert_to_maker(module))
+    elif len(modules) == 1 and _is_iterable(modules[0]):
+      for idx, module in enumerate(modules[0]):
+        setattr(self, str(idx), _convert_to_maker(module))
+    else:
       for idx, module in enumerate(modules):
         setattr(self, str(idx), _convert_to_maker(module))
 
-  def _get_makers(self):
+  def _get_makers(self) -> Dict[str, ILayerMaker]:
     return {key: value for (key, value) in vars(self).items() if isinstance(value, ILayerMaker)}
 
   def append(self, module: _ModT) -> ModuleList:
@@ -63,16 +72,6 @@ class Sequential(ModuleList):
   """
   Sequential Module, takes callable of Modules which are then executed in sequence
   """
-
-  def __init__(self, *modules: Union[_ModT, Dict[str, _ModT]]):
-    super().__init__()
-    if len(modules) == 1 and isinstance(modules[0], dict):
-      for key, module in modules[0].items():
-        setattr(self, key, _convert_to_maker(module))
-    else:
-      for idx, module in enumerate(modules):
-        setattr(self, str(idx), _convert_to_maker(module))
-
   def forward(self, inp) -> LayerRef:
     """
     Forward
@@ -105,3 +104,13 @@ class WrappedFunction(Module):
     Forward
     """
     return self.func(*args, **kwargs)
+
+
+def _is_iterable(obj) -> bool:
+  # No good generic way, so do this ugly hack.
+  # https://stackoverflow.com/questions/1952464/in-python-how-do-i-determine-if-an-object-is-iterable
+  try:
+    iter(obj)
+    return True
+  except TypeError:
+    return False
