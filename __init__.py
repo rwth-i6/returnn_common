cf08b4a2aaacab7187d16ad3534a6d49d674a7b7
Albert Zeyer <albzey@gmail.com>
2021-10-27 14:52:23 +0200

    Lazy module imports
    Via #39

--- __init__.py
+++ __init__.py
@@ -5,7 +5,9 @@ network definition code,
 datasets, etc.
 """
 
+from typing import TYPE_CHECKING as _TYPE_CHECKING
 import sys as _sys
+from ._lazy_loader import LazyLoader as _LazyLoader
 
 # We require at least Python 3.7.
 # See https://github.com/rwth-i6/returnn_common/issues/43.
@@ -13,3 +15,11 @@ import sys as _sys
 # - Our code expects that the order of dict is deterministic, or even insertion order specifically.
 # - Type annotations.
 assert _sys.version_info[:2] >= (3, 7)
+
+# Now all the imports.
+# Use lazy imports, but only when not type checking.
+if _TYPE_CHECKING:
+  from . import nn  # noqa
+
+else:
+  nn = _LazyLoader("nn", globals())
