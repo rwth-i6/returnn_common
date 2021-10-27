41670c0569857ed853527bbe8cf5fe5735d4e2f0
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-10-28 00:28:06 +0200

    Lstm n_out first arg

--- nn/rec.py
+++ nn/rec.py
@@ -9,7 +9,7 @@ class Lstm(Rec):
   """
   LSTM
   """
-  def __init__(self, *, rec_weight_dropout=0, rec_weight_dropout_shape=None, **kwargs):
+  def __init__(self, n_out: int, *, rec_weight_dropout=0, rec_weight_dropout_shape=None, **kwargs):
     assert "unit_opts" not in kwargs, "we handle that here"
     unit_opts = {}
     if rec_weight_dropout:
@@ -19,4 +19,4 @@ class Lstm(Rec):
     if unit_opts:
       kwargs = kwargs.copy()
       kwargs["unit_opts"] = unit_opts
-    super(Lstm, self).__init__(unit="nativelstm2", **kwargs)
+    super(Lstm, self).__init__(n_out=n_out, unit="nativelstm2", **kwargs)
