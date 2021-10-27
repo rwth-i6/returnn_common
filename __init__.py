50f42c39fa8b65513583ce2b21843cc66e15feb6
Albert Zeyer <albzey@gmail.com>
2021-10-27 14:32:30 +0200

    Require Python 3.7
    Fix #43

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
 """
-This is intended to be used for the RETURNN :func:`import_` mechanism.
+Common building blocks for RETURNN,
+such as models or networks,
+network definition code,
+datasets, etc.
 """
+
+import sys as _sys
+
+# We require at least Python 3.7.
+# See https://github.com/rwth-i6/returnn_common/issues/43.
+# Current features we use:
+# - Our code expects that the order of dict is deterministic, or even insertion order specifically.
+# - Type annotations.
+assert _sys.version_info[:2] >= (3, 7)
