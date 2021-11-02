41cab671c260bc8d6081de17a3be1fce854da233
Albert Zeyer <albzey@gmail.com>
2021-11-02 16:27:18 +0100

    test env, try hook into tests

--- tests/_setup_test_env.py
+++ tests/_setup_test_env.py
@@ -108,6 +108,8 @@ def setup():
   import tensorflow as tf
   print("TensorFlow:", tf.__version__)
 
+  _try_hook_into_tests()
+
   # Now some trickery for some good main default.
 
   main_mod = sys.modules.get("__main__")
@@ -115,6 +117,67 @@ def setup():
     main_mod_ = importlib.import_module(
       _expected_package_name + "." + os.path.splitext(os.path.basename(main_mod.__file__))[0])
     _main(main_mod_)
+
+
+def _try_hook_into_tests():
+  """
+  Hook into nosetests or other unittest based frameworks.
+
+  The hook will throw exceptions such that a debugger like PyCharm can inspect them easily.
+  This will only be done if there is just a single test case.
+
+  This code might be a bit experimental.
+  It should work though. But if it does not, we can also skip this.
+  Currently any exception here would be fatal though, as we expect this to work.
+
+  Also see: https://youtrack.jetbrains.com/issue/PY-9848
+  """
+  # Check if this is run inside a debugger. Skip if this is not the case.
+  import sys
+  get_trace = getattr(sys, "gettrace", None)
+  if not get_trace:
+    return
+  if get_trace() is None:
+    return
+  # get TestProgram instance from stack...
+  from unittest import TestProgram
+  from better_exchook import get_current_frame
+  frame = get_current_frame()
+  if not frame:
+    # This will not always work. Just silently accept this. This should be rare.
+    return
+  test_program = None
+  while frame:
+    local_self = frame.f_locals.get("self")
+    if isinstance(local_self, TestProgram):
+      test_program = local_self
+      break
+    frame = frame.f_back
+  if not test_program:
+    # Ok, this is not run as test, so fine, nothing to do then.
+    return
+  test_names = getattr(test_program, "testNames")
+  if not test_names:
+    # Unexpected, but just silently ignore.
+    return
+  if len(test_names) >= 2 or ":" not in test_names[0]:
+    # Multiple tests are being run. Do not hook into this.
+    # We only want to install the hook if there is only a single test case.
+    return
+
+  # Ok, try to install our plugin.
+  class _ReraiseExceptionTestHookPlugin:
+    @staticmethod
+    def _reraise_exception(test, err):
+      exc_class, exc, tb = err
+      print("Test %s, exception %s %s, reraise now." % (test, exc_class.__name__, exc))
+      raise exc
+
+    handleFailure = _reraise_exception
+    handleError = _reraise_exception
+
+  config = getattr(test_program, "config")
+  config.plugins.addPlugin(_ReraiseExceptionTestHookPlugin())
 
 
 def _main(mod):
