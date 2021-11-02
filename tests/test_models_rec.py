45c72ac2079cb48d46602115388bfe0104f916d8
Albert Zeyer <albzey@gmail.com>
2021-11-02 17:52:32 +0100

    rec tests fixes, draft for explicit state
    #31
    #35

--- tests/test_models_rec.py
+++ tests/test_models_rec.py
@@ -8,6 +8,7 @@ from .returnn_helpers import dummy_run_net
 
 from returnn_common.nn import *
 from pprint import pprint
+import unittest
 
 
 def test_rec_ff():
@@ -23,8 +24,33 @@ def test_rec_ff():
       # https://github.com/rwth-i6/returnn_common/issues/16
       with Loop() as loop:
         x_ = loop.unstack(x, axis="T")
+        loop.state.h = State(initial=0)  # TODO proper initial...
         loop.state.h = self.rec_linear(concat(x_, loop.state.h))
         y = loop.stack(loop.state.h)
+      return y
+
+  net = _Net()
+  net_dict = make_root_net_dict(net, "data")
+  pprint(net_dict)
+  dummy_run_net(net_dict)
+
+
+@unittest.skip("TODO ...")
+def test_rec_inner_lstm():
+  class _Net(Module):
+    def __init__(self):
+      super().__init__()
+      self.lstm = Lstm(13)
+
+    def forward(self, x: LayerRef) -> LayerRef:
+      """
+      Forward
+      """
+      with Loop() as loop:
+        x_ = loop.unstack(x, axis="T")
+        loop.state.lstm = State(initial=self.lstm.default_initial_state())
+        y_, loop.state.lstm = self.lstm(x_, state=loop.state.lstm)
+        y = loop.stack(y_)
       return y
 
   net = _Net()
@@ -41,7 +67,7 @@ def test_rec_simple_iter():
       """
       # https://github.com/rwth-i6/returnn_common/issues/16
       with Loop(max_seq_len=10) as loop:
-        loop.state.i = State()
+        loop.state.i = State(initial=0.)
         loop.state.i = loop.state.i + 1.
         loop.end(loop.state.i >= 5.)
         y = loop.stack(loop.state.i * reduce(x, mode="mean", axis="T"))
