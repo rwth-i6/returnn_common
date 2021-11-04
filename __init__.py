94eb824ab6d3cc4c47dd2dee107cc49d2466b2e8
Albert Zeyer <albzey@gmail.com>
2021-11-04 15:03:33 +0100

    maybe easier for PyCharm

--- __init__.py
+++ __init__.py
@@ -5,21 +5,26 @@ network definition code,
 datasets, etc.
 """
 
-from typing import TYPE_CHECKING as _TYPE_CHECKING
-import sys as _sys
-from ._lazy_loader import LazyLoader as _LazyLoader
+import typing
+import sys
+from ._lazy_loader import LazyLoader
 
 # We require at least Python 3.7.
 # See https://github.com/rwth-i6/returnn_common/issues/43.
 # Current features we use:
 # - Our code expects that the order of dict is deterministic, or even insertion order specifically.
 # - Type annotations.
-assert _sys.version_info[:2] >= (3, 7)
+assert sys.version_info[:2] >= (3, 7)
 
 # Now all the imports.
 # Use lazy imports, but only when not type checking.
-if _TYPE_CHECKING:
+if typing.TYPE_CHECKING:
   from . import nn  # noqa
 
 else:
-  nn = _LazyLoader("nn", globals())
+  nn = LazyLoader("nn", globals())
+
+
+del typing
+del sys
+del LazyLoader
