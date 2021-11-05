a570838add83b008c51b55adeeea5119abe2b75f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-11-05 17:23:23 +0100

    Lstm, remove rec_weight_dropout option
    We will handle that in a more generic way (#59).

--- nn/rec.py
+++ nn/rec.py
@@ -9,14 +9,5 @@ class Lstm(_Rec):
   """
   LSTM
   """
-  def __init__(self, n_out: int, *, rec_weight_dropout=0, rec_weight_dropout_shape=None, **kwargs):
-    assert "unit_opts" not in kwargs, "we handle that here"
-    unit_opts = {}
-    if rec_weight_dropout:
-      unit_opts["rec_weight_dropout"] = rec_weight_dropout
-    if rec_weight_dropout_shape:
-      unit_opts["rec_weight_dropout_shape"] = rec_weight_dropout_shape
-    if unit_opts:
-      kwargs = kwargs.copy()
-      kwargs["unit_opts"] = unit_opts
+  def __init__(self, n_out: int, **kwargs):
     super().__init__(n_out=n_out, unit="nativelstm2", **kwargs)
