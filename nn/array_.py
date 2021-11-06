3cc8e4596980b2b14ba619df16378aa58fb413ec
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-11-06 01:31:18 +0100

    split better

--- nn/array_.py
+++ nn/array_.py
@@ -2,7 +2,7 @@
 Array (Tensor) functions
 """
 
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Union
 from returnn.util.basic import NotSpecified
 from .. import nn
 
@@ -36,15 +36,15 @@ def cum_concat_step(
 def split(source: nn.LayerRef, *,
           axis: Optional[str] = NotSpecified,
           num_splits: Optional[int] = NotSpecified,
-          size_splits: Optional[List[int]] = NotSpecified,
-          ) -> Tuple[nn.LayerRef, ...]:
+          size_splits: Optional[Union[List[int], Tuple[int, ...]]] = NotSpecified,
+          name: Optional[str] = None) -> Tuple[nn.LayerRef, ...]:
   """
   Split the input on the specified axis (by default feature).
   Basically a wrapper around tf.split.
   """
   from ._generated_layers import _split
   from .base import get_sub_layer
-  res = _split(source, axis=axis, num_splits=num_splits, size_splits=size_splits)
+  res = _split(source, axis=axis, num_splits=num_splits, size_splits=size_splits, name=name)
   if num_splits is None:
     assert isinstance(size_splits, (tuple, list))
     num_splits = len(size_splits)
