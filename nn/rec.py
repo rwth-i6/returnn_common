97030066a3f0c678e77efde2ce1b8c89e20e2d9f
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2021-12-02 11:28:03 +0100

    added zoneout to rec

--- nn/rec.py
+++ nn/rec.py
@@ -37,3 +37,33 @@ class LSTMStep(_Rec):
     # TODO specify per-step, how? this should also work without rec loop, when there is no time dim.
     #  https://github.com/rwth-i6/returnn/issues/847
     return super().make_layer_dict(source, state=state, axis=None)
+
+
+class ZoneoutLSTM(_Rec):
+  """
+  LSTM with zoneout operating on a sequence. returns (output, final_state) tuple, where final_state is (h,c).
+  """
+  def __init__(self, n_out: int, **kwargs):
+    super().__init__(n_out=n_out, unit="zoneoutlstm", **kwargs)
+
+  # noinspection PyMethodOverriding
+  def make_layer_dict(
+        self, source: nn.LayerRef, *, initial_state: Optional[nn.LayerState] = None) -> nn.LayerDictRaw:
+    """make layer"""
+    return super().make_layer_dict(source, initial_state=initial_state)
+
+
+class ZoneoutLSTMStep(_Rec):
+  """
+  LSTM with zoneout operating one step. returns (output, state) tuple, where state is (h,c).
+  """
+  default_name = "zoneoutlstm"  # make consistent to ZoneoutLSTM
+
+  def __init__(self, n_out: int, **kwargs):
+    super().__init__(n_out=n_out, unit="zoneoutlstm", **kwargs)
+
+  # noinspection PyMethodOverriding
+  def make_layer_dict(
+        self, source: nn.LayerRef, *, state: nn.LayerState) -> nn.LayerDictRaw:
+    """make layer"""
+    return super().make_layer_dict(source, state=state)
