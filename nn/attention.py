dea44d014a5010d94f910305d37eb94d82db7220
Albert Zeyer <albzey@gmail.com>
2021-12-09 16:47:44 +0100

    self attention, nicer code
    Does not work yet that way.
    But we can make it work that way.

--- nn/attention.py
+++ nn/attention.py
@@ -13,16 +13,15 @@ class SelfAttention(nn.Module):
   def __init__(self, *, key_dim_total: nn.Dim, value_dim_total: nn.Dim, num_heads: Union[int, nn.Dim],
                att_dropout: float = 0.):
     super().__init__()
-    self.key_dim_total = key_dim_total
-    self.key_dim_per_head = nn.FeatureDim("key_dim_per_head", key_dim_total.dimension // num_heads)
-    self.value_dim_total = value_dim_total
-    self.value_dim_per_head = nn.FeatureDim("value_dim_per_head", value_dim_total.dimension // num_heads)
     if not isinstance(num_heads, nn.Dim):
       num_heads = nn.SpatialDim("num_heads", num_heads)
+    self.key_dim_total = key_dim_total
+    self.key_dim_per_head = key_dim_total // num_heads
+    self.value_dim_total = value_dim_total
+    self.value_dim_per_head = value_dim_total // num_heads
     self.num_heads = num_heads
-    self.qkv_dim_total = nn.FeatureDim("qkv_total", key_dim_total.dimension * 2 + value_dim_total.dimension)
-    self.qkv_dim_per_head = nn.FeatureDim(
-      "qkv_per_head", self.key_dim_per_head.dimension * 2 + self.value_dim_per_head.dimension)
+    self.qkv_dim_total = key_dim_total * 2 + value_dim_total
+    self.qkv_dim_per_head = self.key_dim_per_head * 2 + self.value_dim_per_head
     self.qkv = nn.Linear(self.qkv_dim_total)
     self.expand_dim = nn.SpatialDim("self_att_expand_dim")
     self.att_dropout = att_dropout
@@ -42,7 +41,7 @@ class SelfAttention(nn.Module):
     v = nn.reinterpret_data(v, set_dim_tags={axis: self.expand_dim}, name="v_new_dim")
     energy = nn.dot([q, k], reduce=self.key_dim_per_head, var1=axis, var2=self.expand_dim, name="energy")
     att_weights = nn.softmax(energy, axis=self.expand_dim, name="att_weights")
-    att_weights = nn.dropout(att_weights, self.att_dropout)  # TODO wrong axis
+    att_weights = nn.dropout(att_weights, self.att_dropout, axis=self.expand_dim)
     att = nn.dot(
       [att_weights, v], reduce=self.expand_dim, var1=axis, var2=self.value_dim_per_head, name="att")
     output = nn.merge_dims(
@@ -83,7 +82,7 @@ class SelfAttentionStep(nn.Module):
     energy = nn.dot(
       [q, k_accum], reduce=self.key_dim_per_head, var1=None, var2=self.expand_dim, name="energy")
     att_weights = nn.softmax(energy, axis=self.expand_dim, name="att_weights")
-    att_weights = nn.dropout(att_weights, self.att_dropout)
+    att_weights = nn.dropout(att_weights, self.att_dropout, axis=self.expand_dim)
     att = nn.dot(
       [att_weights, v_accum],
       reduce=self.expand_dim, var1=None, var2=self.value_dim_per_head, name="att")
