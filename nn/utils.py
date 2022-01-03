432110cc4dbd818e26c6a5ac595ffc621da98171
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2022-01-03 12:57:19 +0100

    nn.dropout simplify

--- nn/utils.py
+++ nn/utils.py
@@ -2,42 +2,39 @@
 Some generic utils (which doesn't fit into math_, array_, etc)
 """
 
-from typing import Optional, Dict, Union
+from typing import Optional, Union, Sequence
 from .. import nn
-from ..nn import NotSpecified
 
 
 # noinspection PyShadowingNames
 def dropout(source: nn.LayerRef,
             dropout: float,
             *,
-            axis: nn.Dim = NotSpecified,
-            noise_shape: Dict[Union[str, nn.Dim], Optional[int]] = NotSpecified,
+            axis: Union[nn.Dim, Sequence[nn.Dim]],
             on_forward: bool = False,
             name: Optional[str] = None
             ) -> nn.LayerRef:
   """
   Applies dropout.
+
   Dropout will only be applied during training (unless you set on_forward=True).
 
+  When dropout is applied, the output will be scaled by 1/dropout.
+
   :param nn.LayerRef source:
-  :param float dropout: 0.0 means to apply no dropout.
-  :param Dim axis:
-  :param dict[str|nn.Dim,int|None] noise_shape: see :func:`returnn.tf.util.data.get_bc_shape`
+  :param float dropout: 0.0 means to apply no dropout. 100% would mask everything.
+    For every value in the tensor, the probability of it being dropped is drawn independently given this probability.
+    The broadcasted axes are those not specified in ``axis``.
+  :param axis: axis to apply dropout on. multiple axes can be specified.
+    This defines the set of axes where the dropout mask is not broadcasted to.
+    (RETURNN also has the ``noise_shape`` option but the ``axis`` option provides the same functionality.)
   :param bool on_forward: apply dropout during inference
   :param str|None name:
   """
   assert isinstance(source, nn.LayerRef)
   if not dropout:
     return source
-  opts = {"dropout": dropout}
-  if axis is not NotSpecified:
-    assert noise_shape is NotSpecified, "cannot provide both axis and noise_shape"
-    opts["dropout_axis"] = axis
-  elif noise_shape is not NotSpecified:
-    opts["dropout_noise_shape"] = noise_shape
-  else:
-    raise ValueError("dropout: provide either `axis` or `noise_shape` explicitly")
+  opts = {"dropout": dropout, "dropout_axis": axis}
   if on_forward:
     opts["dropout_on_forward"] = True
   from .base import make_layer
