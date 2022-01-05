72a03d2de8168c54fa31c051905870a34a8a2b56
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2022-01-05 13:04:42 +0100

    BatchNorm more

--- nn/norm.py
+++ nn/norm.py
@@ -28,13 +28,46 @@ class BatchNorm(nn.Module):
 
   Note that the default arguments differ from corresponding batch norm in RETURNN.
   See here for discussion on defaults: https://github.com/rwth-i6/returnn/issues/522
+
+  We calculate statistics over all axes except the given in_dim.
+  I.e. all other axes are reduced for the statistics.
+
+  To compensate the normalization, there are learnable parameters gamma and beta
+  (optional, used when option `affine` is True).
+
+  The usual behavior depends on whether this is used in training or evaluation,
+  although this often configurable in other frameworks.
+  The usual behavior, in training::
+
+      # Using statistics from current batch.
+      mean_cur_batch, variance_cur_batch = moments(source, reduce_dims)
+      y = (x - mean_cur_batch) / sqrt(variance_cur_batch + epsilon)
+      y = gamma * y + beta
+
+      # Updating running statistics for later use.
+      mean = (1 - momentum) * mean + momentum * mean_cur_batch
+      variance = (1 - momentum) * variance + momentum * variance_cur_batch
+
+  The usual behavior, not in training (i.e. in evaluation)::
+
+      # Using collected statistics. Not using statistics from current batch.
+      y = (x - mean) / sqrt(variance + epsilon)
+      y = gamma * y + beta
+
   """
 
-  def __init__(self, in_dim: Optional[nn.Dim] = None):
+  def __init__(self, in_dim: Optional[nn.Dim] = None, *, affine: bool = True):
+    """
+    :param in_dim: the feature dimension of the input
+    :param affine: whether to use learnable parameters gamma and beta
+    """
     super().__init__()
     self.in_dim = in_dim
     self.mean = None  # type: Optional[nn.Parameter]
     self.var = None  # type: Optional[nn.Parameter]
+    self.affine = affine
+    self.gamma = None  # type: Optional[nn.Parameter]
+    self.beta = None  # type: Optional[nn.Parameter]
     if in_dim:
       self._lazy_init(in_dim)
 
@@ -42,13 +75,16 @@ class BatchNorm(nn.Module):
     self.in_dim = in_dim
     self.mean = nn.Parameter([in_dim], trainable=False)
     self.var = nn.Parameter([in_dim], trainable=False)
+    if self.affine:
+      self.gamma = nn.Parameter([in_dim])
+      self.beta = nn.Parameter([in_dim])
 
   def __call__(self, source: nn.LayerRef, *,
                epsilon: float = 1e-5) -> nn.Layer:
     source = nn.check_in_feature_dim_lazy_init(source, self.in_dim, self._lazy_init)
     reduce_dims = [d for d in source.data.dim_tags if d != self.in_dim]
-    mean, variance = moments(source, reduce_dims)
-    mean.verify_out_shape({self.in_dim})
-    variance.verify_out_shape({self.in_dim})
+    mean_cur_batch, variance_cur_batch = moments(source, reduce_dims)
+    mean_cur_batch.verify_out_shape({self.in_dim})
+    variance_cur_batch.verify_out_shape({self.in_dim})
     # TODO: handle running mean/var ...
-    return (source - mean) * nn.rsqrt(variance + epsilon)
+    return (source - mean_cur_batch) * nn.rsqrt(variance_cur_batch + epsilon)
