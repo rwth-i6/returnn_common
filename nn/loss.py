f0f48ed418545e27dcda5a64c93a5c6a00a7dcd3
Albert Zeyer <albzey@gmail.com>
2022-01-13 18:02:15 +0100

    cross entropy, small restructure

--- nn/loss.py
+++ nn/loss.py
@@ -43,14 +43,15 @@ def cross_entropy(*, target: nn.LayerRef, estimated: nn.LayerRef, estimated_type
   if not axis:
     assert target.feature_dim
     axis = target.feature_dim
+  if estimated_type == "logits" and target.data.sparse:
+    # This is a common case and TF provides an optimized function for it, so use that directly.
+    return nn.sparse_softmax_cross_entropy_with_logits(logits=estimated, targets=target, axis=axis)
   if estimated_type == "probs":
-    return -nn.dot(target, nn.safe_log(estimated), reduce=axis)
+    log_prob = nn.safe_log(estimated)
   elif estimated_type == "log-probs":
-    return -nn.dot(target, estimated, reduce=axis)
+    log_prob = estimated
   elif estimated_type == "logits":
-    if target.data.sparse:
-      # This is a common case and TF provides an optimized function for it, so use that directly.
-      return nn.sparse_softmax_cross_entropy_with_logits(logits=estimated, targets=target, axis=axis)
-    return -nn.dot(target, nn.log_softmax(estimated, axis=axis), reduce=axis)
+    log_prob = nn.log_softmax(estimated, axis=axis)
   else:
     raise ValueError("estimated_kind must be 'probs', 'log-probs' or 'logits'")
+  return -nn.dot(target, log_prob, reduce=axis)
