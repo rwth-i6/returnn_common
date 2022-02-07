1aaec96b5161bfc1b0e4ba87b8b2eebcdd397f1e
Albert Zeyer <albzey@gmail.com>
2022-02-07 10:12:05 +0100

    test loop, test param name

--- tests/test_nn_loop.py
+++ tests/test_nn_loop.py
@@ -37,7 +37,11 @@ def test_rec_ff():
       return y
 
   config, net_dict = dummy_config_net_dict(net=_Net(), with_axis=True)
-  dummy_run_net(config)
+  engine = dummy_run_net(config)
+  params = engine.network.get_params_list()
+  print(params)
+  assert len(params) == 2
+  assert_equal(params[0].name, "rec_linear/bias/param:0")
 
 
 def test_lstm_default_name():
