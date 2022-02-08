8a48d4bd866ba4e15bf3bcef0e446acda2ad8121
Albert Zeyer <albzey@gmail.com>
2022-02-08 10:15:35 +0100

    ones

--- nn/const.py
+++ nn/const.py
@@ -19,3 +19,18 @@ def zeros_like(value: nn.Tensor, *, name: Optional[Union[str, nn.NameCtx]] = Non
   zeros with shape and dtype from value. But there is no dependency on value in the computation graph.
   """
   return zeros(shape=value.data.dim_tags, dtype=value.dtype, name=name)
+
+
+def ones(shape: Sequence[nn.Dim], dtype: Optional[str] = nn.NotSpecified,
+         *, name: Optional[Union[str, nn.NameCtx]] = None) -> nn.Tensor:
+  """
+  ones
+  """
+  return nn.constant(value=1., shape=shape, dtype=dtype, name=name or "ones")
+
+
+def ones_like(value: nn.Tensor, *, name: Optional[Union[str, nn.NameCtx]] = None) -> nn.Tensor:
+  """
+  ones with shape and dtype from value. But there is no dependency on value in the computation graph.
+  """
+  return ones(shape=value.data.dim_tags, dtype=value.dtype, name=name)
