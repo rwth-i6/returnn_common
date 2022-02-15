ba30922719d1959dd2df91167225aa6f79381d5b
Albert Zeyer <albzey@gmail.com>
2022-02-15 11:12:52 +0100

    test_nn_transformer_search fixes

--- tests/test_nn_transformer.py
+++ tests/test_nn_transformer.py
@@ -17,9 +17,11 @@ else:
 def test_nn_transformer_search():
   with nn.NameCtx.new_root() as name_ctx:
     time_dim = nn.SpatialDim("time")
-    input_dim = nn.FeatureDim("input", 4)
+    input_dim = nn.FeatureDim("input", 10)
     data = nn.get_extern_data(nn.Data("data", dim_tags=[nn.batch_dim, time_dim, input_dim]))
-    transformer = nn.Transformer()
+    transformer = nn.Transformer(
+      output_dim=input_dim, dim_ff=input_dim * 4,
+      num_heads=2, num_encoder_layers=2, num_decoder_layers=2)
     out, _ = transformer(data, source_spatial_axis=time_dim, search=True, beam_size=3, eos_symbol=0, name=name_ctx)
     out.mark_as_default_output()
 
