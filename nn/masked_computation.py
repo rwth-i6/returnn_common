masked computation, fix and extend draft

nn/masked_computation.py
@@ -10,19 +10,7 @@ class MaskedComputation:
   """
   This is expected to be inside a :class:`Loop`.
 
-  Example without nn.MaskedComputation::
-
-      loop = nn.Loop(...)
-      loop.state.y = ...  # some initial output
-      loop.state.h = ...  # some initial state
-      with loop:
-
-        mask = ...  # dtype bool, shape [batch] or whatever, for current frame
-        y, loop.state.h = slow_rnn(x, loop.state.h)
-        y = nn.where(cond=mask, x=y, y=loop.state.y)
-        loop.state.y = y
-
-  Is equivalent to::
+  Usage example::
 
       loop = nn.Loop(...)
       loop.state.y = ...  # some initial output
@@ -31,7 +19,30 @@ class MaskedComputation:
 
         mask = ...  # dtype bool, shape [batch] or whatever, for current (fast) frame
         with nn.MaskedComputation(mask=mask) as masked_comp:
-          y, loop.state.h = slow_rnn(x, loop.state.h)
-        y, loop.state.y = masked_comp.unmask(y, loop.state.y)  # access from outside
+          loop.state.y, loop.state.h = slow_rnn(x, loop.state.h)
+        y = loop.state.y  # access from outside
+
+  This is equivalent to::
+
+      loop = nn.Loop(...)
+      loop.state.y = ...  # some initial output
+      loop.state.h = ...  # some initial state
+      with loop:
+
+        mask = ...  # dtype bool, shape [batch] or whatever, for current frame
+        y_, h_ = slow_rnn(x, loop.state.h)
+        loop.state.y = nest.map(lambda a, b: nn.where(cond=mask, x=a, y=b), y_, loop.state.y)
+        loop.state.h = nest.map(lambda a, b: nn.where(cond=mask, x=a, y=b), h_, loop.state.h)
+        y = loop.state.y
+
+  In pseudocode, non-batched (mask is just a scalar bool), it would look like::
+
+      y = ...  # some initial output
+      h = ...  # some initial state
+      while True:
+
+        mask = ...  # bool
+        if mask:
+          y, h = slow_rnn(x, h)
 
   """