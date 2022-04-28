b9632364b34f7cbb8e9b3e1c0b90f3288606174e
Albert Zeyer <albzey@gmail.com>
2022-04-28 02:39:12 +0200

    test_gt_gammatone_v2 extend

--- tests/test_asr_gt.py
+++ tests/test_asr_gt.py
@@ -24,3 +24,6 @@ def test_gt_gammatone_v2():
   mod = gt.GammatoneV2()
   audio, audio_spatial_dim = mod(raw_audio, in_spatial_dim=raw_audio_spatial_dim)
   assert audio.data.placeholder is not None
+  audio_, out_spatial_dim_ = gt.gammatone_v1(raw_audio, normalization=None)
+  assert audio_.data.placeholder is not None
+  assert (audio_.data.placeholder.numpy() == audio.data.placeholder.numpy()).all()
