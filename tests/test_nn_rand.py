1558ec5a6e50ee8177378760cf4e3585238727cc
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-05-05 14:06:53 +0200

    test_random_multi_call
    #148

--- tests/test_nn_rand.py
+++ tests/test_nn_rand.py
@@ -30,3 +30,24 @@ def test_random_normal():
   config, net_dict = dummy_config_net_dict(net, reset_name_ctx=False)
   pprint(net_dict)
   dummy_run_net(config, net=net)
+
+
+def test_random_multi_call():
+  # https://github.com/rwth-i6/returnn_common/issues/148
+  # Actually we will not really test the non-determinism as this is difficult to test.
+  # We just test whether we can run it multiple times without error.
+  nn.reset_default_root_name_ctx()
+
+  class _Net(nn.Module):
+    def __init__(self):
+      super().__init__()
+      self.rnd = nn.Random()
+
+    @nn.scoped
+    def __call__(self, x: nn.Tensor) -> nn.Tensor:
+      return x + self.rnd.normal(x.shape_ordered) - self.rnd.normal(x.shape_ordered)
+
+  net = _Net()
+  config, net_dict = dummy_config_net_dict(net, reset_name_ctx=False)
+  pprint(net_dict)
+  dummy_run_net(config, net=net)
