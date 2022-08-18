555a6c6ea49156a6f22c82bd49f3679ec9c7eba1
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-08-18 16:30:18 +0200

    decoder intf more

--- nn/decoder/base.py
+++ nn/decoder/base.py
@@ -16,8 +16,9 @@ TODO this is all work-in-progress. the transducer-fullsum was the base for this 
 """
 
 from __future__ import annotations
-from typing import Optional
+from typing import Optional, Tuple
 from enum import Enum
+import dataclasses
 from ... import nn
 
 
@@ -86,6 +87,109 @@ class Decoder(nn.Module):
 
 # TODO enc ctx module
 # TODO make generic type for args (Generic, TypeVar) to support not just single layer?
+
+
+class IDecoderJointLogProbOutput:
+  """
+  What :class:`IDecoderJointLogProb` returns.
+  """
+
+  @property
+  def blank_idx(self) -> int:
+    """
+    :return: blank index
+    """
+    raise NotImplementedError
+
+  @property
+  def log_prob_wb(self) -> nn.Tensor:
+    """
+    :return: shape (...,D) where is number of classes *with* blank, log probs
+    """
+    assert self.__class__.log_prob_nb is not IDecoderJointLogProbOutput.log_prob_nb
+    log_prob_nb = self.log_prob_nb
+    log_prob_blank = nn.expand_dim(self.log_prob_blank, dim=nn.FeatureDim("blank", 1))
+    log_prob_nb += self.log_prob_not_blank
+    if self.blank_idx == 0:
+      log_prob_wb = nn.concat((log_prob_blank, log_prob_blank.feature_dim), (log_prob_nb, log_prob_nb.feature_dim))
+    elif self.blank_idx == log_prob_nb.feature_dim:
+      log_prob_wb = nn.concat((log_prob_nb, log_prob_nb.feature_dim), (log_prob_blank, log_prob_blank.feature_dim))
+    else:
+      raise NotImplementedError(f"blank idx {self.blank_idx}, dims (excl blank) {log_prob_nb.feature_dim}")
+    return log_prob_wb
+
+  @property
+  def log_prob_nb(self) -> nn.Tensor:
+    """
+    :return: shape (...,D) where is number of classes *without* blank, log probs
+    """
+    assert self.__class__.log_prob_wb is not IDecoderJointLogProbOutput.log_prob_wb
+    log_prob_wb = self.log_prob_wb
+    if self.blank_idx == 0:
+      log_prob_nb, _ = nn.slice(log_prob_wb, axis=log_prob_wb.feature_dim, slice_start=1)
+    elif self.blank_idx == log_prob_wb.feature_dim - 1:
+      log_prob_nb, _ = nn.slice(log_prob_wb, axis=log_prob_wb.feature_dim, slice_end=-1)
+    else:
+      raise NotImplementedError(f"blank idx {self.blank_idx}, dims (incl blank) {log_prob_wb.feature_dim}")
+    log_prob_nb -= self.log_prob_not_blank
+    return log_prob_nb
+
+  @property
+  def log_prob_blank(self) -> nn.Tensor:
+    """
+    :return: log prob of blank
+    """
+    assert self.__class__.log_prob_wb is not IDecoderJointLogProbOutput.log_prob_wb
+    log_prob_wb = self.log_prob_wb
+    return nn.gather(log_prob_wb, position=self.blank_idx, axis=log_prob_wb.feature_dim)
+
+  @property
+  def log_prob_not_blank(self) -> nn.Tensor:
+    """
+    :return: log prob of not blank -> log prob of emitting a non-blank label
+    """
+    log_prob_blank = self.log_prob_blank
+    return nn.safe_log(-nn.expm1(log_prob_blank))
+
+
+@dataclasses.dataclass
+class DecoderJointLogProbSeparatedOutput(IDecoderJointLogProbOutput):
+  """
+  blank separated
+  """
+  blank_idx: int
+  log_prob_nb: nn.Tensor
+  log_prob_blank: nn.Tensor
+  log_prob_not_blank: nn.Tensor  # log(-expm1(log_prob_blank)) but you maybe could calc it more directly
+
+
+class IDecoderJointNoStateLogProb(nn.Module):
+  """
+  Joint network for transducer-like models:
+
+  getting in step-sync inputs, label-sync inputs,
+  producing probabilities for labels + blank
+  """
+
+  def __call__(self, *, step_sync_in: nn.Tensor, label_sync_in: nn.Tensor) -> IDecoderJointLogProbOutput:
+    raise NotImplementedError
+
+
+class IDecoderJointStateLogProb(nn.Module):
+  """
+  Joint network for transducer-like models:
+
+  getting in step-sync inputs, label-sync inputs,
+  producing probabilities for labels + blank
+  """
+
+  def __call__(self, *,
+               step_sync_in: nn.Tensor,
+               step_sync_state: nn.LayerState,
+               label_sync_in: nn.Tensor,
+               label_sync_state: nn.LayerState,
+               ) -> Tuple[IDecoderJointLogProbOutput, nn.LayerState]:
+    raise NotImplementedError
 
 
 class IDecoderLabelSyncRnn(nn.Module):
