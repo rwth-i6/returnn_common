a38b305042bcbcbf27eb4037661e658046b04df4
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-08-18 13:07:42 +0200

    blstm (initial, wip)

---
+++ nn/encoder/blstm.py
@@ -0,0 +1,65 @@
+"""
+Multi layer BLSTm
+"""
+
+from typing import Union, Tuple
+from ... import nn
+
+
+class BlstmEncoder(nn.Module):
+  """
+  multi-layer BLSTM
+  """
+  def __init__(self,
+               num_layers: int = 6, dim: nn.Dim = nn.FeatureDim("lstm-dim", 1024),
+               time_reduction: Union[int, Tuple[int, ...]] = 6,
+               l2=0.0001, dropout=0.3, rec_weight_dropout=0.0,
+               ):
+    super(BlstmEncoder, self).__init__()
+    self.num_layers = num_layers
+
+    if isinstance(time_reduction, int):
+      n = time_reduction
+      time_reduction = []
+      for i in range(2, n + 1):
+        while n % i == 0:
+          time_reduction.insert(0, i)
+          n //= i
+        if n <= 1:
+          break
+    assert isinstance(time_reduction, (tuple, list))
+    while len(time_reduction) > num_layers - 1:
+      time_reduction[:2] = [time_reduction[0] * time_reduction[1]]
+    self.time_reduction = time_reduction
+
+    self.dropout = dropout
+    self.rec_weight_dropout = rec_weight_dropout
+
+    self.layers = nn.ModuleList([BlstmSingleLayer(dim=dim) for _ in range(num_layers)])
+
+  def __call__(self, x: nn.Tensor, *, spatial_dim: nn.Dim) -> nn.Tensor:
+    for i, lstm in enumerate(self.layers):
+      if i > 0:
+        red = self.time_reduction[i - 1] if (i - 1) < len(self.time_reduction) else 1
+        if red > 1:
+          x = nn.pool1d(x, mode="max", padding="same", pool_size=red, in_spatial_dim=spatial_dim)
+        if self.dropout:
+          x = nn.dropout(x, dropout=self.dropout, axis=x.feature_dim)
+      assert isinstance(lstm, BlstmSingleLayer)
+      x = lstm(x, axis=spatial_dim)
+    return x
+
+
+class BlstmSingleLayer(nn.Module):
+  """
+  single-layer BLSTM
+  """
+  def __init__(self, dim: nn.Dim):
+    super(BlstmSingleLayer, self).__init__()
+    self.fw = nn.LSTM(out_dim=dim)
+    self.bw = nn.LSTM(out_dim=dim)
+
+  def __call__(self, x: nn.Tensor, *, axis: nn.Dim) -> nn.Tensor:
+    fw, _ = self.fw(x, axis=axis, direction=1)
+    bw, _ = self.bw(x, axis=axis, direction=-1)
+    return nn.concat((fw, fw.feature_dim), (bw, bw.feature_dim))
---
+++ nn/encoder/blstm_cnn_specaug.py
@@ -0,0 +1,59 @@
+"""
+Common BLSTM-based encoder often used for end-to-end (attention, transducer) models:
+
+SpecAugment . PreCNN . BLSTM
+"""
+
+
+from typing import Union, Tuple
+
+from ...asr.specaugment import specaugment_v2
+from ... import nn
+from .blstm import BlstmEncoder
+
+
+class BlstmCnnSpecAugEncoder(BlstmEncoder):
+  """
+  SpecAugment . PreCNN . BLSTM
+  """
+
+  def __init__(self,
+               num_layers: int = 6, lstm_dim: nn.Dim = nn.FeatureDim("lstm-dim", 1024),
+               time_reduction: Union[int, Tuple[int, ...]] = 6,
+               with_specaugment=True,
+               l2=0.0001, dropout=0.3, rec_weight_dropout=0.0,):
+    super(BlstmCnnSpecAugEncoder, self).__init__(
+      num_layers=num_layers, dim=lstm_dim, time_reduction=time_reduction,
+      l2=l2, dropout=dropout, rec_weight_dropout=rec_weight_dropout)
+
+    self.with_specaugment = with_specaugment
+    self.pre_conv_net = PreConvNet()
+
+  def __call__(self, x, *, spatial_dim: nn.Dim) -> nn.Tensor:
+    if self.with_specaugment:
+      x = specaugment_v2(x, spatial_dim=spatial_dim)
+    x = self.pre_conv_net(x, spatial_dim=spatial_dim)
+    x = super(BlstmCnnSpecAugEncoder, self).__call__(x, spatial_dim=spatial_dim)
+    return x
+
+
+class PreConvNet(nn.Module):
+  """
+  2 layer pre conv net, usually used before a BLSTM
+  """
+  def __init__(self, filter_size=(3, 3), dim: nn.Dim = nn.FeatureDim("feat", 32)):
+    super(PreConvNet, self).__init__()
+    self.conv0 = nn.Conv2d(out_dim=dim, padding="same", filter_size=filter_size)
+    self.conv1 = nn.Conv2d(out_dim=dim, padding="same", filter_size=filter_size)
+
+  def __call__(self, x: nn.Tensor, *, spatial_dim: nn.Dim) -> nn.Tensor:
+    extra_spatial_dim = x.feature_dim
+    feat_dim = nn.FeatureDim("dummy-feature", 1)
+    x = nn.split_dims(x, axis=x.feature_dim, dims=(extra_spatial_dim, feat_dim))
+    x, _ = self.conv0(x, in_spatial_dims=(spatial_dim, extra_spatial_dim), in_dim=feat_dim)
+    feat_dim = x.feature_dim
+    x, extra_spatial_dim = nn.pool1d(x, in_spatial_dim=extra_spatial_dim, pool_size=2, mode="max", padding="same")
+    x, _ = self.conv1(x, in_spatial_dims=(spatial_dim, extra_spatial_dim), in_dim=feat_dim)
+    x, extra_spatial_dim = nn.pool1d(x, in_spatial_dim=extra_spatial_dim, pool_size=2, mode="max", padding="same")
+    x, _ = nn.merge_dims(x, axes=(extra_spatial_dim, feat_dim), out_dim=nn.FeatureDim("conv-net-feature", None))
+    return x
