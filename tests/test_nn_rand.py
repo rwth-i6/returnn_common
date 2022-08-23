0c38fe1d7859ae41bb1146b852bdd223b6b1d87e
Albert Zeyer <albzey@gmail.com>
2022-08-23 23:47:08 +0200

    test_random_normal_shape_get_network

--- tests/test_nn_rand.py
+++ tests/test_nn_rand.py
@@ -82,3 +82,74 @@ def test_random_in_loop():
   out0_np = out_np[:1]  # [1,B,D]
   print((out0_np == out_np))
   assert not (out0_np == out_np).all()  # not all the same
+
+
+def test_random_normal_shape_get_network():
+  # https://github.com/rwth-i6/returnn_common/issues/197
+  class _Net(nn.Module):
+    def __call__(self, x_: nn.Tensor) -> nn.Tensor:
+      return x_ + nn.random_normal(x_.shape_ordered)
+
+  from returnn.config import Config
+  from returnn.tf.engine import Engine
+  in_dim = nn.FeatureDim("in", 3)
+  x = nn.Data("data", dim_tags=[nn.batch_dim, in_dim], available_for_inference=True)
+
+  def _config_get_network(epoch: int, **_kwargs) -> dict:
+    # noinspection PyStatementEffect
+    epoch  # unused
+    nn.reset_default_root_name_ctx()
+    net = _Net()
+    # net = nn.Linear(out_dim)
+    out = net(nn.get_extern_data(x))
+    out.mark_as_default_output()
+    out.mark_as_loss()
+    net_dict = nn.get_returnn_config().get_net_dict_raw_dict(net)
+    pprint(net_dict)
+    return net_dict
+
+  config = Config({
+    "task": "train", "num_epochs": 1, "start_epoch": 1,
+    "get_network": _config_get_network,
+    "extern_data": {x.name: {"dim_tags": [nn.batch_dim, in_dim], "available_for_inference": True}},
+  })
+  engine = Engine(config)
+  engine.init_train_from_config(config)
+
+
+def test_random_normal_shape_get_network_with_time():
+  # https://github.com/rwth-i6/returnn_common/issues/197
+  class _Net(nn.Module):
+    def __call__(self, x_: nn.Tensor) -> nn.Tensor:
+      return x_ + nn.random_normal(x_.shape_ordered)
+
+  from returnn.config import Config
+  from returnn.tf.engine import Engine
+  from returnn.datasets import init_dataset
+  time_dim = nn.SpatialDim("time")
+  in_dim = nn.FeatureDim("in", 3)
+  x = nn.Data("data", dim_tags=[nn.batch_dim, time_dim, in_dim], available_for_inference=True)
+
+  def _config_get_network(epoch: int, **_kwargs) -> dict:
+    # noinspection PyStatementEffect
+    epoch  # unused
+    nn.reset_default_root_name_ctx()
+    net = _Net()
+    # net = nn.Linear(out_dim)
+    out = net(nn.get_extern_data(x))
+    out.mark_as_default_output()
+    out.mark_as_loss()
+    net_dict = nn.get_returnn_config().get_net_dict_raw_dict(net)
+    return net_dict
+
+  config = Config({
+    "task": "train", "num_epochs": 1, "start_epoch": 1,
+    "get_network": _config_get_network,
+    "extern_data": {x.name: {"dim_tags": [nn.batch_dim, time_dim, in_dim], "available_for_inference": True}},
+  })
+  train_dataset = init_dataset(
+    {"class": "DummyDataset", "input_dim": in_dim.dimension, "output_dim": 5, "num_seqs": 3})
+  engine = Engine(config)
+  engine.init_train_from_config(config, train_data=train_dataset)
+  res = engine.forward_single(dataset=train_dataset, seq_idx=0)
+  print(res)
