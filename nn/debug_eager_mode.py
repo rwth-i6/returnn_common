96987f8a3aa40d35af832f988601b94719101b9a
Albert Zeyer <albzey@gmail.com>
2022-08-23 15:49:43 +0200

    disable_debug_eager_mode

--- nn/debug_eager_mode.py
+++ nn/debug_eager_mode.py
@@ -19,6 +19,19 @@ def enable_debug_eager_mode():
   _debug_eager_mode_enabled = True
 
 
+def disable_debug_eager_mode():
+  """
+  For debugging.
+
+  Enables TF eager mode.
+  Also, all layers will directly be created, and then due to TF eager mode directly evaluated.
+  """
+  global _debug_eager_mode_enabled
+  import tensorflow as tf
+  tf.compat.v1.disable_eager_execution()
+  _debug_eager_mode_enabled = False
+
+
 def is_debug_eager_mode_enabled() -> bool:
   """
   :return: True if debug eager mode is enabled.
