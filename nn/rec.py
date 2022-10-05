1e44765e4ddf945c95a06531671fe232b4bd5efe
Albert Zeyer <albzey@gmail.com>
2022-10-05 23:37:54 +0200

    Rec, extend API for in_dim

--- nn/rec.py
+++ nn/rec.py
@@ -13,8 +13,8 @@ class _Rec(nn.Module):
   See :func:`__call__`.
   """
 
-  def __init__(self, *, out_dim: nn.Dim, unit: str, param_list: List[Tuple[str, Callable[[], Tuple[nn.Dim, ...]]]],
-               in_dim: Optional[nn.Dim] = None,
+  def __init__(self, in_dim: nn.Dim, out_dim: nn.Dim, *,
+               unit: str, param_list: List[Tuple[str, Callable[[], Tuple[nn.Dim, ...]]]],
                unit_opts: Optional[Dict[str, Any]] = None):
     """
     :param out_dim: dimension tag for the output feature dimension
@@ -25,25 +25,16 @@ class _Rec(nn.Module):
     :param unit_opts: additional options for the recurrent unit
     """
     super().__init__()
-    self.out_dim = out_dim
     self.in_dim = in_dim
+    self.out_dim = out_dim
     self.unit = unit
     self.unit_opts = unit_opts
     self.param_list = param_list
-    if in_dim:
-      self._lazy_init(in_dim)
-
-  def _lazy_init(self, in_dim: nn.Dim):
-    assert in_dim
-    if self.in_dim:
-      assert self.in_dim == in_dim
-    else:
-      self.in_dim = in_dim
-      for param, shape_func in self.param_list:
-        shape = shape_func()
-        param_ = nn.Parameter(shape)
-        param_.initial = nn.init.Glorot()
-        setattr(self, f"param_{param}", param_)
+    for param, shape_func in self.param_list:
+      shape = shape_func()
+      param_ = nn.Parameter(shape)
+      param_.initial = nn.init.Glorot()
+      setattr(self, f"param_{param}", param_)
 
   def __call__(self, source: nn.Tensor, *,
                axis: nn.Dim,
@@ -57,7 +48,6 @@ class _Rec(nn.Module):
     :param direction: 1 for forward direction, -1 for backward direction
     :return: out, out_state. out_state is the new or last state.
     """
-    self._lazy_init(source.feature_dim)
     rec_layer_dict = {
       "class": "rec", "from": source,
       "in_dim": self.in_dim, "axis": axis, "out_dim": self.out_dim,
@@ -95,13 +85,13 @@ class LSTM(_Rec):
   """
   LSTM. returns (output, state) tuple, where state is (h,c).
   """
-  def __init__(self, out_dim: nn.Dim, *, in_dim: Optional[nn.Dim] = None):
+  def __init__(self, in_dim: nn.Dim, out_dim: nn.Dim):
     self.param_W_re = None  # type: Optional[nn.Parameter]
     self.param_W = None  # type: Optional[nn.Parameter]
     self.param_b = None  # type: Optional[nn.Parameter]
     super().__init__(
+      in_dim=in_dim, out_dim=out_dim,
       unit="nativelstm2",
-      out_dim=out_dim, in_dim=in_dim,
       param_list=[
         ("W_re", lambda: (self.out_dim, 4 * self.out_dim)),
         ("W", lambda: (self.in_dim, 4 * self.out_dim)),
@@ -112,15 +102,13 @@ class ZoneoutLSTM(_Rec):
   """
   LSTM with zoneout. returns (output, state) tuple, where state is (h,c).
   """
-  def __init__(self, out_dim: nn.Dim,
-               *,
-               in_dim: Optional[nn.Dim] = None,
+  def __init__(self, in_dim: nn.Dim, out_dim: nn.Dim, *,
                zoneout_factor_cell: float = 0., zoneout_factor_output: float = 0.):
     self.param_kernel = None  # type: Optional[nn.Parameter]
     self.param_bias = None  # type: Optional[nn.Parameter]
     super().__init__(
+      in_dim=in_dim, out_dim=out_dim,
       unit="zoneoutlstm",
-      out_dim=out_dim, in_dim=in_dim,
       unit_opts={'zoneout_factor_cell': zoneout_factor_cell, 'zoneout_factor_output': zoneout_factor_output},
       param_list=[
         ("kernel", lambda: (self.in_dim + self.out_dim, 4 * self.out_dim)),
