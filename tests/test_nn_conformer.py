ee0d79dd5eaeb6b6c8d4c3dca299a71037ddf656
Albert Zeyer <albzey@gmail.com>
2022-10-06 20:07:28 +0200

    test small fix warning cleanup

--- tests/test_nn_conformer.py
+++ tests/test_nn_conformer.py
@@ -7,7 +7,6 @@ from __future__ import annotations
 from . import _setup_test_env  # noqa
 from .returnn_helpers import dummy_run_net, config_net_dict_via_serialized
 from .utils import assert_equal
-from tensorflow.python.util import nest
 import typing
 
 if typing.TYPE_CHECKING:
