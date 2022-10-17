452028b78ac8b76f05dc2084cc20df3e5e51567b
Albert Zeyer <albzey@gmail.com>
2022-10-17 16:55:33 +0200

    test_nn_conformer small fix

--- tests/utils.py
+++ tests/utils.py
@@ -8,4 +8,11 @@ def assert_equal(a, b):
   :param T a:
   :param T b:
   """
-  assert a == b, f"{a!r} != {b!r}"
+  assert type(a) == type(b), f"types differ: {type(a)} != {type(b)}"
+  if a == b:
+    return
+  res = ["a != b", f"a: {a}", f"b: {b}"]
+  if isinstance(a, set):
+    assert isinstance(b, set)
+    res += [f"a - b: {a - b}", f"b - a: {b - a}"]
+  raise Exception("\n".join(res))
