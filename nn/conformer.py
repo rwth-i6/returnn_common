4c34e4c3c38023c73498989249fc105375d7321c
Albert Zeyer <albzey@gmail.com>
2022-10-20 14:04:51 +0200

    ConformerEncoder, change default num_heads to 4

--- nn/conformer.py
+++ nn/conformer.py
@@ -253,7 +253,7 @@ class ConformerEncoder(nn.Module):
                dropout: float = 0.1,
                conv_kernel_size: int = 32,
                conv_norm: Union[nn.BatchNorm, Any] = nn.NotSpecified,
-               num_heads: int = 8,
+               num_heads: int = 4,
                att_dropout: float = 0.1,
                custom_encoder_layer: Optional[Union[ConformerEncoderLayer, Any]] = None):
     """
