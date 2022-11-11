ed0363d411a914b2fd29f42c004165b4fcde6021
Albert Zeyer <albzey@gmail.com>
2022-11-11 16:21:43 +0100

    param init interface better

--- nn/init.py
+++ nn/init.py
@@ -12,7 +12,7 @@ from .. import nn
 class ParamInit:
   """API for param init"""
   def __call__(self, shape: Sequence[nn.Dim], dtype: str) -> nn.Tensor:
-    ...
+    raise NotImplementedError
 
 
 class VarianceScaling(ParamInit):
