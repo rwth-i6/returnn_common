2c3c9ad935b00aef1bdf584858e2d0b6720c49b6
Albert Zeyer <albzey@gmail.com>
2022-11-14 16:48:21 +0100

    switchout

---
+++ nn/utils/switchout.py
@@ -0,0 +1,24 @@
+"""
+Switchout, see :func:`switchout`.
+"""
+
+from __future__ import annotations
+from ... import nn
+
+
+def switchout(
+  source: nn.Tensor,
+  switchout_prob: float,
+  *,
+  on_forward: bool = False,
+) -> nn.Tensor:
+  """
+  Switchout - similar as dropout (:func:`dropout`) but on class labels, randomly replace by some other class label.
+  """
+  if not source.data.sparse_dim:
+    raise ValueError(f"switchout only works on sparse data, got {source}")
+  with nn.Cond(nn.train_flag() | on_forward) as cond:
+    random_label = nn.random_label(source.shape_ordered, source.data.sparse_dim, dtype=source.dtype)
+    cond.true = nn.where(nn.random_uniform(source.shape_ordered, maxval=1.) < switchout_prob, random_label, source)
+    cond.false = source
+  return cond.result
