c762762466b315b710142938ab89ec0c712ecf7f
Albert Zeyer <albzey@gmail.com>
2022-11-16 21:57:53 +0100

    bump min behavior version
    https://github.com/rwth-i6/returnn/pull/1222

--- nn/base.py
+++ nn/base.py
@@ -67,7 +67,7 @@ NetDictRaw = Dict[str, LayerDictRaw]
 RawTensorTypes = Union[int, float, complex, numpy.number, numpy.ndarray, bool, str]
 OutShapeType = Union[Set[Union[Dim, _MarkedDim]], tuple, list]
 
-min_returnn_behavior_version = 14
+min_returnn_behavior_version = 16
 
 
 class Tensor:
