48e651d2bce5c062fc1457d8cdda67149fa6fc33
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2022-11-16 10:19:00 +0000

    label_smoothed_log_prob_gradient

--- nn/utils/label_smoothing.py
+++ nn/utils/label_smoothing.py
@@ -47,3 +47,51 @@ def smooth_one_hot(source: nn.Tensor, *, label_prob: Union[nn.Tensor, float]) ->
     raise NotImplementedError(f"smooth_one_hot({source}) not implemented for dynamic dims")
   return nn.sparse_to_dense(
     source, label_value=label_prob, other_value=(1. - label_prob) / (source.data.sparse_dim.dimension - 1))
+
+
+def label_smoothed_log_prob_gradient(log_prob: nn.Tensor, smoothing: Union[nn.Tensor, float],
+                                     *, axis: Optional[nn.Dim] = None) -> nn.Tensor:
+  """
+  :param log_prob: shape [...,D] (not necessarily the same as loss)
+  :param smoothing: smoothing factor, for :func:`label_smoothing`
+  :param axis: label axis. uses feature_dim by default
+
+  Assume some cross-entropy-like loss:
+
+    loss = - sum_i target_prob[i] * log_prob[i] .
+
+  The sum is over the label indices i (corresponding to the ``axis`` argument).
+  Then the gradient of loss w.r.t. log_prob[i] is:
+
+    grad_logprob[i] loss = -target_prob[i] .
+
+  We assume that the negative gradient is a probability distribution, and apply :func:`label_smoothing` on it.
+  More specifically, we apply the same scale and shift as in the :func:`label_smoothing` function
+  via :func:`scaled_gradient`.
+
+  Just as a side remark: assume
+
+    log_prob = log_softmax(z) .
+
+  The gradient of log_softmax is:
+
+    grad_z[j] log_prob[i] = delta(i==j) - softmax(z)[j] .
+
+  Then the gradient w.r.t. z[j] is:
+
+    grad_z[j] loss = sum_i (grad_logprob[i] loss) (grad_z[j] logprob[i])
+                   = sum_i -target_prob[i] delta(i==j) + target_prob[i] softmax(z)[j]
+                   = -target_prob[j] + (sum_i target_prob[i]) softmax(z)[j]
+                   = softmax(z)[j] - target_prob[j]    # assuming (sum_i target_prob[i]) == 1
+                                                    .
+
+  """
+  if not axis:
+    assert log_prob.feature_dim
+    axis = log_prob.feature_dim
+  # See formula above for label_smoothing.
+  dim = axis.dimension
+  floor_prob = smoothing / (dim - 1)
+  factor = 1. - dim * floor_prob
+  # This is expected to be the negative target prob, thus negative floor_prob.
+  return nn.scaled_gradient(log_prob, scale=factor, shift=-floor_prob)
