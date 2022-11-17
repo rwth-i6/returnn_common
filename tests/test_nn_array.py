d5408a35f3db0888c35105c99275efea0bcb090a
Albert Zeyer <albzey@gmail.com>
2022-11-17 14:27:14 +0100

    test_window_stride

--- tests/test_nn_array.py
+++ tests/test_nn_array.py
@@ -42,12 +42,18 @@ def test_concat_features():
   dummy_run_net_single_custom(config_str, eval_flag=True)
 
 
-def naive_window(source: numpy.ndarray, window: int, padding='same'):
+def ceildiv(a, b):
+  """ceildiv"""
+  return -(-a // b)
+
+
+def naive_window(source: numpy.ndarray, window: int, stride: int = 1, padding='same'):
   """
   naive implementation of tf_util.windowed
 
   :param source: (time,...)
   :param window: window size
+  :param stride: stride
   :param padding: 'same' or 'valid'
   :return: (time,window,...)
   """
@@ -67,10 +73,10 @@ def naive_window(source: numpy.ndarray, window: int, padding='same'):
   pad_left = numpy.zeros((w_left,) + source.shape[1:], dtype=dtype)
   pad_right = numpy.zeros((w_right,) + source.shape[1:], dtype=dtype)
   padded = numpy.concatenate([pad_left, source, pad_right], axis=0)
-  final = numpy.zeros((n_time, window) + source.shape[1:], dtype=dtype)
-  for t in range(n_time):
-    for w in range(window):
-      final[t, w] = padded[t + w]
+  final = numpy.zeros((ceildiv(n_time, stride), window) + source.shape[1:], dtype=dtype)
+  for t in range(final.shape[0]):
+    for w in range(final.shape[1]):
+      final[t, w] = padded[t * stride + w]
   return final
 
 
@@ -86,3 +92,17 @@ def test_window():
   config_str = nn.get_returnn_config().get_config_raw_dict(nn.Module())
   res = dummy_run_net_single_custom(config_str, default_out_dim_tag_order=[time_dim, win_dim, nn.batch_dim, in_dim])
   numpy.testing.assert_array_equal(res["layer:output"], naive_window(res["data:data"], win_dim.dimension))
+
+
+def test_window_stride():
+  nn.reset_default_root_name_ctx()
+  time_dim = nn.SpatialDim("time")
+  in_dim = nn.FeatureDim("in", 3)
+  x = nn.Data("data", dim_tags=[time_dim, nn.batch_dim, in_dim], available_for_inference=True)
+  x = nn.get_extern_data(x)
+  win_dim = nn.SpatialDim("window", 5)
+  out, time_dim_ = nn.window(x, axis=time_dim, window_dim=win_dim, stride=3)
+  out.mark_as_default_output()
+  config_str = nn.get_returnn_config().get_config_raw_dict(nn.Module())
+  res = dummy_run_net_single_custom(config_str, default_out_dim_tag_order=[time_dim_, win_dim, nn.batch_dim, in_dim])
+  numpy.testing.assert_array_equal(res["layer:output"], naive_window(res["data:data"], win_dim.dimension, 3))
