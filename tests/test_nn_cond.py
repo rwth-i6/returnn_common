1d5b6fd1b27ec6201890935201a22ece60149c2a
Albert Zeyer <albzey@gmail.com>
2022-11-22 15:42:39 +0100

    test_cond_new_axis

--- tests/test_nn_cond.py
+++ tests/test_nn_cond.py
@@ -4,7 +4,7 @@ Test nn.cond
 from __future__ import annotations
 
 from . import _setup_test_env  # noqa
-from .returnn_helpers import dummy_run_net, dummy_config_net_dict, dummy_default_in_dim
+from .returnn_helpers import dummy_run_net, dummy_config_net_dict, dummy_run_net_single_custom, dummy_default_in_dim
 
 import typing
 
@@ -94,3 +94,27 @@ def test_cond_random():
 
   config, net_dict, net = dummy_config_net_dict(_Net)
   dummy_run_net(config, net=net)
+
+
+def test_cond_new_axis():
+  # Like in SelfAttention.
+  nn.reset_default_root_name_ctx()
+  in_dim = nn.FeatureDim("in", 12)
+  time_dim = nn.SpatialDim("time")
+  x = nn.get_extern_data(nn.Data("data", dim_tags=[nn.batch_dim, time_dim, in_dim]))
+  net = nn.Linear(in_dim, in_dim)
+  axis = time_dim
+
+  with nn.Cond(nn.dim_value(nn.batch_dim) % 2 == 0) as cond:
+    x_ = x
+    x_ = net(x_)
+    new_dim = nn.SpatialDim(f"{axis.description}:new-dim")
+    x_, _ = nn.reinterpret_new_dim(x_, in_dim=axis, out_dim=new_dim)
+    x_ = net(x_)
+    cond.true = nn.reduce(x_, axis=new_dim, mode="max")
+    cond.false = nn.reduce(x, axis=axis, mode="max")
+  y = cond.result
+  y.mark_as_default_output()
+
+  config_str = nn.get_returnn_config().get_complete_py_code_str(net)
+  dummy_run_net_single_custom(config_str)
