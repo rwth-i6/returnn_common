195e59fd5b8af94002ae92ece8481b78e71a1fa7
Albert Zeyer <albzey@gmail.com>
2022-11-23 12:54:09 +0100

    test_cond_chunking_conformer (#249)
    This PR doesn't really change anything except adding the test case.
    I think the test case is still useful as a slightly more complex test case.
    The underlying problem which this test case was reproducing was fixed on RETURNN side via rwth-i6/returnn#1232.

--- tests/test_nn_cond.py
+++ tests/test_nn_cond.py
@@ -4,7 +4,8 @@ Test nn.cond
 from __future__ import annotations
 
 from . import _setup_test_env  # noqa
-from .returnn_helpers import dummy_run_net, dummy_config_net_dict, dummy_run_net_single_custom, dummy_default_in_dim
+from .returnn_helpers import dummy_run_net, dummy_config_net_dict, dummy_run_net_single_custom, \
+  config_net_dict_via_serialized, dummy_default_in_dim
 
 import typing
 
@@ -118,3 +119,111 @@ def test_cond_new_axis():
 
   config_str = nn.get_returnn_config().get_complete_py_code_str(net)
   dummy_run_net_single_custom(config_str)
+
+
+def test_cond_chunking_conformer():
+  # The test says "chunking conformer" but we reduced it as much as possible
+  # while still reproducing the issue, and there is no chunking and also no conformer anymore.
+  # However, the nn.Cond still seems to be very relevant for the issue.
+  # But also, the nn.Loop later seems relevant.
+  from typing import Tuple
+
+  class Encoder(nn.Module):
+    """encoder"""
+    def __init__(self, in_dim: nn.Dim):
+      super(Encoder, self).__init__()
+      self.self_att = nn.RelPosSelfAttention(
+        in_dim, proj_dim=None, key_dim_total=nn.FeatureDim("keys", 2), value_dim_total=in_dim, num_heads=1)
+      self.out_dim = in_dim
+
+    def __call__(self, x: nn.Tensor, *, in_spatial_dim: nn.Dim) -> Tuple[nn.Tensor, nn.Dim]:
+      x = self.self_att(x, axis=in_spatial_dim)
+      return x, in_spatial_dim
+
+  class Model(nn.Module):
+    """Model definition"""
+
+    def __init__(self, in_dim: nn.Dim, *,
+                 nb_target_dim: nn.Dim,
+                 wb_target_dim: nn.Dim,
+                 blank_idx: int,
+                 ):
+      super(Model, self).__init__()
+      self.in_dim = in_dim
+      self.encoder = Encoder(in_dim)
+
+      self.nb_target_dim = nb_target_dim
+      self.wb_target_dim = wb_target_dim
+      self.blank_idx = blank_idx
+
+      self.out_label_logits = nn.Linear(self.encoder.out_dim, wb_target_dim)
+
+    def encode(self, source: nn.Tensor, *, in_spatial_dim: nn.Dim,
+               ) -> Tuple[nn.Tensor, nn.Dim]:
+      """encode, and extend the encoder output for things we need in the decoder"""
+      enc, enc_spatial_dim = source, in_spatial_dim
+      with nn.Cond(nn.train_flag()) as cond:
+        enc_, _ = self.encoder(enc, in_spatial_dim=enc_spatial_dim)
+        cond.true = enc_
+        enc_, _ = self.encoder(enc, in_spatial_dim=enc_spatial_dim)
+        cond.false = enc_
+      enc = cond.result
+      return enc, enc_spatial_dim
+
+    def decode(self,
+               enc: nn.Tensor,  # single frame if axis is single step, or sequence otherwise ("am" before)
+               ) -> nn.Tensor:
+      """decoder step, or operating on full seq"""
+      logits = self.out_label_logits(enc)
+      return logits
+
+  def model_recog(*,
+                  model: Model,
+                  data: nn.Tensor, data_spatial_dim: nn.Dim,
+                  targets_dim: nn.Dim,  # noqa
+                  ) -> nn.Tensor:
+    """
+    Function is run within RETURNN.
+
+    Earlier we used the generic beam_search function,
+    but now we just directly perform the search here,
+    as this is overall simpler and shorter.
+
+    :return: recog results including beam
+    """
+    batch_dims = data.batch_dims_ordered((data_spatial_dim, data.feature_dim))
+    enc, enc_spatial_dim = model.encode(data, in_spatial_dim=data_spatial_dim)
+    beam_size = 3
+
+    loop = nn.Loop(axis=enc_spatial_dim)  # time-sync transducer
+    loop.max_seq_len = nn.dim_value(enc_spatial_dim) * 2  # WHAT? this triggers it?
+    loop.state.target = nn.constant(model.blank_idx, shape=batch_dims, sparse_dim=model.wb_target_dim)
+    with loop:
+      enc = loop.unstack(enc)
+      logits = model.decode(enc)
+      log_prob = nn.log_softmax(logits, axis=model.wb_target_dim)
+      loop.state.target = nn.choice(
+        log_prob, input_type="log_prob",
+        target=None, search=True, beam_size=beam_size,
+        length_normalization=False)
+      res = loop.stack(loop.state.target)
+    return res
+
+  nn.reset_default_root_name_ctx()
+  time_dim = nn.SpatialDim("time")
+  input_dim = nn.FeatureDim("input", 10)
+  label_dim = nn.FeatureDim("labels", 5)
+  data = nn.get_extern_data(nn.Data("data", dim_tags=[nn.batch_dim, time_dim, input_dim]))
+
+  model = Model(
+    input_dim,
+    nb_target_dim=label_dim,
+    wb_target_dim=label_dim + 1,
+    blank_idx=label_dim.dimension,
+  )
+  model_recog(model=model, data=data, data_spatial_dim=time_dim, targets_dim=label_dim).mark_as_default_output()
+
+  # config = nn.get_returnn_config().get_config_raw_dict(root_module=model)
+  config_code = nn.get_returnn_config().get_complete_py_code_str(model)
+  config, net_dict = config_net_dict_via_serialized(config_code)
+  dummy_run_net(config, net=model)
