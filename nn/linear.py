c7816f1afb41fd486cfcf8ee366602f58ec7149a
Albert Zeyer <albzey@gmail.com>
2022-11-23 12:56:38 +0100

    Linear, raise better exception on incorrect type

--- nn/linear.py
+++ nn/linear.py
@@ -24,6 +24,8 @@ class Linear(nn.Module):
       self.bias.initial = 0.
 
   def __call__(self, source: nn.Tensor) -> nn.Tensor:
+    if not isinstance(source, nn.Tensor):
+      raise TypeError(f"{self}: source must be a Tensor but got {type(source)}")
     if self.in_dim not in source.shape and self.in_dim != source.data.sparse_dim:
       raise ValueError(f"{self}: input {source} does not have in_dim {self.in_dim}")
     out = nn.dot(source, self.weight, reduce=self.in_dim)
