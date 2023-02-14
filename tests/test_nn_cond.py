462f102e14b88685bff5d287124d40273d3dd718
Albert Zeyer <albzey@gmail.com>
2023-02-14 23:00:04 +0100

    test_cond_dim

--- tests/test_nn_cond.py
+++ tests/test_nn_cond.py
@@ -10,9 +10,11 @@ from .returnn_helpers import (
     dummy_run_net_single_custom,
     config_net_dict_via_serialized,
     dummy_default_in_dim,
+    make_feed_dict,
 )
 
 import typing
+import functools
 
 if typing.TYPE_CHECKING:
     from .. import nn
@@ -124,6 +126,48 @@ def test_cond_new_axis():
 
     config_str = nn.get_returnn_config().get_complete_py_code_str(net)
     dummy_run_net_single_custom(config_str)
+
+
+def test_cond_dim():
+    nn.reset_default_root_name_ctx()
+    in_dim = nn.FeatureDim("in", 12)
+    time_dim = nn.SpatialDim("time")
+    spatial_dim = time_dim
+    x = nn.get_extern_data(nn.Data("data", dim_tags=[nn.batch_dim, time_dim, in_dim]))
+    out_spatial_dim = spatial_dim - 1 + spatial_dim
+    clipping = 2
+    clipped_spatial_dim = nn.SpatialDim(f"learned-rel-pos", dimension=2 * clipping + 1)
+    mat_spatial_size = clipping + 1
+    pos_emb = nn.random_uniform((clipped_spatial_dim, in_dim), maxval=1.0)
+
+    # Example via LearnedRelativePositionalEncoding.
+    with nn.Cond(nn.dim_value(spatial_dim) > mat_spatial_size) as cond:
+        # True branch
+        left = nn.gather(pos_emb, axis=clipped_spatial_dim, position=0)
+        right = nn.gather(pos_emb, axis=clipped_spatial_dim, position=clipped_spatial_dim.dimension - 1)
+        remaining_dim = spatial_dim - mat_spatial_size
+        left = nn.expand_dim(left, dim=remaining_dim)
+        right = nn.expand_dim(right, dim=remaining_dim)
+        concat, out_spatial_dim_ = nn.concat(
+            (left, remaining_dim), (pos_emb, clipped_spatial_dim), (right, remaining_dim)
+        )
+        concat, out_spatial_dim_ = nn.replace_dim(concat, in_dim=out_spatial_dim_, out_dim=out_spatial_dim)
+        cond.true = concat
+
+        # False branch, spatial_dim <= self.clipping
+        cond.false, _ = nn.slice_nd(
+            pos_emb,
+            axis=clipped_spatial_dim,
+            start=mat_spatial_size - nn.dim_value(spatial_dim),
+            size=out_spatial_dim,
+        )
+
+    y = cond.result
+    y = y + nn.reduce(x, axis=(time_dim, nn.batch_dim), mode="mean")
+    y.mark_as_default_output()
+
+    config_str = nn.get_returnn_config().get_complete_py_code_str(nn.Module())
+    dummy_run_net_single_custom(config_str, make_feed_dict=functools.partial(make_feed_dict, n_time=1))
 
 
 def test_cond_multiple_outputs():
