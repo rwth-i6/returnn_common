1a86664c074ff735ea852290ab0e438e88ecbf73
Albert Zeyer <albzey@gmail.com>
2023-02-15 01:14:09 +0100

    test_prev_target_seq, small fix
    This is not guaranteed anymore,
    as we want to avoid declare_same_as,
    and in general
    any such implicit assumptions.

--- tests/test_nn_utils.py
+++ tests/test_nn_utils.py
@@ -20,7 +20,6 @@ def test_prev_target_seq():
     x = nn.Data("data", dim_tags=[nn.batch_dim, time_dim], sparse_dim=in_dim, available_for_inference=True)
     x = nn.get_extern_data(x)
     out, dim = nn.prev_target_seq(x, spatial_dim=time_dim, bos_idx=0, out_one_longer=True)
-    assert dim == 1 + time_dim
     out.mark_as_default_output()
     config_str = nn.get_returnn_config().get_complete_py_code_str(nn.Module())
     dummy_run_net_single_custom(config_str, eval_flag=True)
