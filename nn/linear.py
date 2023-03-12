1bfdf29013f4188d155070eeae3279946bdff3c3
Albert Zeyer <albzey@gmail.com>
2023-03-12 17:07:42 +0100

    Embedding separate from Linear

--- nn/linear.py
+++ nn/linear.py
@@ -26,9 +26,31 @@ class Linear(nn.Module):
     def __call__(self, source: nn.Tensor) -> nn.Tensor:
         if not isinstance(source, nn.Tensor):
             raise TypeError(f"{self}: source must be a Tensor but got {type(source)}")
-        if self.in_dim not in source.dims_set and self.in_dim != source.sparse_dim:
+        if self.in_dim not in source.dims_set:
             raise ValueError(f"{self}: input {source} does not have in_dim {self.in_dim}")
         out = nn.dot(source, self.weight, reduce=self.in_dim)
         if self.with_bias:
             out += self.bias
+        return out
+
+
+class Embedding(nn.Module):
+    """
+    Embedding.
+    """
+
+    def __init__(self, in_dim: nn.Dim, out_dim: nn.Dim):
+        super().__init__()
+        assert isinstance(in_dim, nn.Dim) and isinstance(out_dim, nn.Dim)
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.weight = nn.Parameter((nn.dim_match_priority_when_needed(self.in_dim, self.out_dim), self.out_dim))
+        self.weight.initial = nn.init.Glorot()
+
+    def __call__(self, source: nn.Tensor) -> nn.Tensor:
+        if not isinstance(source, nn.Tensor):
+            raise TypeError(f"{self}: source must be a Tensor but got {type(source)}")
+        if self.in_dim != source.sparse_dim:
+            raise ValueError(f"{self}: input {source} does not have in_dim {self.in_dim}")
+        out = nn.gather(self.weight, position=source, axis=self.in_dim)
         return out
